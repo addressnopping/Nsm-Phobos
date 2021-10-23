9963d125703a7866f01e5645b485fd5ef904311f
Peterrr__ <pedrohenriquebm1108@gmail.com>
2021-10-23 17:22:39 -0300

    CSGOWatermark module added and version rename

--- src/main/java/me/earth/phobos/Phobos.java
+++ src/main/java/me/earth/phobos/Phobos.java
@@ -22,11 +22,11 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 
-@Mod(modid = "nsm", name = "Nsm Phobos", version = "2.0.2")
+@Mod(modid = "nsm", name = "Nsm Phobos", version = Phobos.MODVER)
 public class Phobos {
     public static final String MODID = "nsm";
     public static final String MODNAME = "Nsm Phobos";
-    public static final String MODVER = "2.0.2";
+    public static final String MODVER = "2.0.3";
     public static final String NAME_UNICODE = "3\u1d00\u0280\u1d1b\u029c\u029c4\u1d04\u1d0b";
     public static final String PHOBOS_UNICODE = "Nsm \u1d18\u029c\u1d0f\u0299\u1d0f\ua731";
     public static final String CHAT_SUFFIX = " \u23d0 3\u1d00\u0280\u1d1b\u029c\u029c4\u1d04\u1d0b";
@@ -195,7 +195,7 @@ public class Phobos {
         if (moduleManager.getModuleByClass(CustomMainScreen.class).isEnabled()) {
             customMainScreen = new GuiCustomMainScreen();
         }
-        Display.setTitle("Nsm Phobos - v2.0.2");
+        Display.setTitle("Nsm Phobos - v" + Phobos.MODVER);
         Phobos.load();
     }
 }
