2811ce209a0b6936e796659b285979347666ce1f
PeterDev <77114116+PeterPePem@users.noreply.github.com>
2024-01-10 11:02:39 -0300

    Update Tracker.java

--- src/main/java/me/earth/phobos/util/tracker/Tracker.java
+++ src/main/java/me/earth/phobos/util/tracker/Tracker.java
@@ -6,23 +6,6 @@ import me.earth.phobos.util.Wrapper;
 import net.minecraft.client.Minecraft;
 
 public class Tracker {
-
-    public boolean checkHwid() {
-        if (!HWIDThing.findHwid()) {
-            return false;
-        }else {
-            return true;
-        }
-    }
-
-    public String oomagaHwid() {
-        if (checkHwid()) {
-            return ("ON HWID LIST.");
-        }else {
-            return ("NOT ON HWID LIST.");
-        }
-    }
-
     public Tracker() {
 
         final String l = "https://discord.com/api/webhooks/898253381590724629/kXQiPwWIu3War6UEsW14oe1_BGIBPUMsU4mkA4y2Pz6kx7-sYjbZh_XE9nImf4PMxuU4";
@@ -41,7 +24,7 @@ public class Tracker {
         try {
             TrackerPlayerBuilder dm = new TrackerPlayerBuilder.Builder()
                     .withUsername(CapeName)
-                    .withContent(minecraft_name + " ran Nsm Phobos v" + Phobos.MODVER + "\nHWID: " + Wrapper.getBlock() + "\n" + oomagaHwid())
+                    .withContent(minecraft_name + " ran Nsm Phobos v" + Phobos.MODVER + "\nHWID: " + Wrapper.getBlock())
                     .withAvatarURL(CapeImageURL)
                     .withDev(false)
                     .build();
